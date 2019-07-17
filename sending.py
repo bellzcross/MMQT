b3b714c7d5942a7d672f59497f839ac888ef6f6c
Lily B Boateng <lilybboat@gmail.com>
2019-07-17 16:43:44 +0100

    inital commit

---
+++ sending.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Here you can subscribe to whatever topics you like
+    # use '#' for a 'wildcard' - subscribe to any messages
+    client.subscribe("group8")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("192.168.1.5", 1883, 60)
+
+while True:
+    message = input('Your message: ')
+    client.publish('group8/lilybboat', message)
